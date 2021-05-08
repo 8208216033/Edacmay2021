91f6195632b548461823b97d80e27ec2562ec1a6
PunamSontakke <83824742+8208216033@users.noreply.github.com>
2021-05-08 14:30:28 +0530

    Add files via upload

---
+++ Assignment1/Q3.java
@@ -0,0 +1,17 @@
+//Dividation of two numbers
+import java.util.Scanner;
+class Q3
+{
+public static void main(String args[])
+{
+int n1,n2,division;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter n1");
+n1=sc.nextInt();
+System.out.println("Enter n2");
+n2=sc.nextInt();
+division=n1/n2;
+System.out.println("Division="+division);
+
+}
+}
