b396bf6f93a4248d5a5ebef43c4e7be0fff458f9
PunamSontakke <83824742+8208216033@users.noreply.github.com>
2021-05-08 14:36:20 +0530

    Add files via upload

---
+++ Assignment1/Q9.java
@@ -0,0 +1,10 @@
+//solve Expression and print answer
+class Q9
+{
+public static void main(String args[])
+{
+float n1=25.5f,n2=3.5f,n3=40.5f,n4=4.5f;
+
+System.out.println((n1*n2-n2*n2)/(n3-n4));
+}
+}
