69a0294b1764e74dca8b94f58ce5aa335e09862d
PunamSontakke <83824742+8208216033@users.noreply.github.com>
2021-05-08 14:45:34 +0530

    Add files via upload

---
+++ Assignment1/Q16.java
@@ -0,0 +1,16 @@
+//Print faca pattern
+import java.util.Scanner;
+class Q16
+{
+public static void main(String args[])
+{
+
+System.out.println(" +\"\"\"\"\"+ ");
+System.out.println("[| o o |]");
+System.out.println(" |  ^  | ");
+System.out.println(" | '-' |");
+System.out.println(" +-----+");
+
+
+}
+}
