2ca54eb0c6dc8bb894139ab54c3c6fc5051d5be6
PunamSontakke <83824742+8208216033@users.noreply.github.com>
2021-05-08 14:37:52 +0530

    Add files via upload

---
+++ Assignment1/Q11.java
@@ -0,0 +1,15 @@
+//peramiter of circle
+import java.util.Scanner;
+class Q11
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+float r=7.5f;
+double pi=3.14,perimeter,area; 
+perimeter=2*pi*r;
+System.out.println("Perimter="+perimeter);
+area=pi*r*r;
+System.out.println("Area="+area);
+}
+}
