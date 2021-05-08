ce617223990d172e3d29eacac86b0c7832eb1edf
PunamSontakke <83824742+8208216033@users.noreply.github.com>
2021-05-08 14:41:47 +0530

    Add files via upload

---
+++ Assignment1/Q14.java
@@ -0,0 +1,19 @@
+//Print American Flag
+import java.util.Scanner;
+class Q14
+{
+public static void main(String args[])
+{
+String P1="* * * * * * = = = = = = = = = = = = = = = = = = = = = = = = = = = ";
+String P2="= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ";
+
+for(int i=1;i<=4;i++)
+{
+System.out.println(P1);
+}
+for(int i=1;i<=6;i++)
+{
+System.out.println(P2);
+}
+}
+}
