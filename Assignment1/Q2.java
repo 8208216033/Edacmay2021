8da4df1dc2cc5260b281446cd11615b736ce9891
PunamSontakke <83824742+8208216033@users.noreply.github.com>
2021-05-08 14:30:02 +0530

    Add files via upload

---
+++ Assignment1/Q2.java
@@ -0,0 +1,17 @@
+//Addition of two number
+import java.util.Scanner;
+class Q2
+{
+public static void main(String args[])
+{
+int n1,n2,sum;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter n1");
+n1=sc.nextInt();
+System.out.println("Enter n2");
+n2=sc.nextInt();
+sum=n1+n2;
+System.out.println("Addition="+sum);
+
+}
+}
