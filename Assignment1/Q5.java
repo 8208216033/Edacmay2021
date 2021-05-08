fadf92fd57cb27a6757d759ef0925754e83bcf45
PunamSontakke <83824742+8208216033@users.noreply.github.com>
2021-05-08 14:39:49 +0530

    Add files via upload

---
+++ Assignment1/Q5.java
@@ -0,0 +1,17 @@
+//Multiplication of two numbers
+import java.util.Scanner;
+class Q5
+{
+public static void main(String args[])
+{
+int n1,n2,Multiplication;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter n1");
+n1=sc.nextInt();
+System.out.println("Enter n2");
+n2=sc.nextInt();
+Multiplication=n1*n2;
+System.out.println("Multiplication="+Multiplication);
+
+}
+}
